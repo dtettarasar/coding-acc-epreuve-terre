563c0140c7cb6a2395f608962259c18541b8a575
Dylan Tettarasar <dtettarasar@gmail.com>
2022-03-14 11:36:16 -0400

    update on terre00

---
+++ terre00.js
@@ -0,0 +1,12 @@
+const alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+// console.log(alphabet);
+
+let str = "";
+
+
+for (let i = 0; i < alphabet.length; i++) {
+    str += alphabet[i]
+}
+
+
+console.log(str);
