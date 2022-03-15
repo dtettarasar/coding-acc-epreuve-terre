32f67311697ce09afa1625a0d5b6f12e86484c01
Dylan Tettarasar <dtettarasar@gmail.com>
2022-03-15 12:40:46 -0400

    terre03: created getRemainingLetters function

--- terre03.js
+++ terre03.js
@@ -3,13 +3,25 @@
 const args = process.argv.slice(2);
 const alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
 const pattern = /^[a-zA-Z]{1}$/;
-let letterFound = false;
 
+const argTester = pattern.test(args[0]);
+
+/*
 console.log(args)
-console.log(pattern.test(args[0]));
+console.log(argTester);
+*/
 
-if (args.length != 1) {
+const getRemainingLetters = (letter) => {
+    let letterFound = false;
+    const container = [];
+    console.log(letter);
+    return container;
+}
+
+if (args.length != 1 || !argTester) {
     console.log("Veuillez saisir une lettre");
     console.log("Exemple : node terre03.js a");
     return false;
+} else {
+    getRemainingLetters(args[0]);
 }
