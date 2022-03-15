edf2e53030ef4df091744d15e1a53ea1b9f59d1f
Dylan Tettarasar <dtettarasar@gmail.com>
2022-03-15 12:30:58 -0400

    terre03 : add condition & regex to test argument

--- terre03.js
+++ terre03.js
@@ -1,12 +1,12 @@
 //Exercice 4 : L’alphabet à partir de
 
 const args = process.argv.slice(2);
-
 const alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
-
+const pattern = /^[a-zA-Z]{1}$/;
 let letterFound = false;
 
 console.log(args)
+console.log(pattern.test(args[0]));
 
 if (args.length != 1) {
     console.log("Veuillez saisir une lettre");
