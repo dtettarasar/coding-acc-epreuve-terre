bb6337aadce4e1aa130d47ed79788834d3c997fd
Dylan Tettarasar <dtettarasar@gmail.com>
2022-03-19 18:59:00 +0100

    terre05 : created argTester and main func

--- terre05.js
+++ terre05.js
@@ -3,21 +3,38 @@
 const args = process.argv.slice(2);
 const pattern = /^-?[0-9]+$/;
 
-const testDividende = pattern.test(args[0]);
-const testDiviseur = pattern.test(args[1]);
+const main = () => {
+
+  if (argTester()) {
+    divider(args[0], args[1]);
+  }
+
+}
 
 const divider = (dividende, diviseur) => {
 
-  console.log("dividende: " + dividende);
-  console.log("diviseur: " + diviseur);
+  const intDividende = parseInt(dividende);
+  const intDiviseur = parseInt(diviseur);
+
+  console.log("dividende: " + intDividende);
+  console.log("diviseur: " + intDiviseur);
 
 }
 
-if (args.length != 2 || !testDividende || !testDiviseur) {
-    console.log("Veuillez passer deux nombres entiers en argument (le dividende, puis le diviseur)");
-    console.log("Exemple : node terre05.js 10 2");
-    return false;
-} else {
-    divider(args[0], args[1]);
-    return true;
+const argTester = () => {
+
+  const testDividende = pattern.test(args[0]);
+  const testDiviseur = pattern.test(args[1]);
+
+  if (args.length != 2 || !testDividende || !testDiviseur) {
+      console.log("Veuillez passer deux nombres entiers en argument (le dividende, puis le diviseur)");
+      console.log("Exemple : node terre05.js 10 2");
+      return false;
+  } else {
+      //divider(args[0], args[1]);
+      return true;
+  }
+
 }
+
+main();
