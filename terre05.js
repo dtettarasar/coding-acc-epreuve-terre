ffaa4ee6bb78de04e4927b0452a588ca4cc4d58a
Dylan Tettarasar <dtettarasar@gmail.com>
2022-03-19 18:47:12 +0100

    terre05 : created divider func

--- terre05.js
+++ terre05.js
@@ -6,11 +6,18 @@ const pattern = /^-?[0-9]+$/;
 const testDividende = pattern.test(args[0]);
 const testDiviseur = pattern.test(args[1]);
 
+const divider = (dividende, diviseur) => {
+
+  console.log("dividende: " + dividende);
+  console.log("diviseur: " + diviseur);
+
+}
+
 if (args.length != 2 || !testDividende || !testDiviseur) {
     console.log("Veuillez passer deux nombres entiers en argument (le dividende, puis le diviseur)");
     console.log("Exemple : node terre05.js 10 2");
     return false;
 } else {
-    console.log(args)
+    divider(args[0], args[1]);
     return true;
 }
