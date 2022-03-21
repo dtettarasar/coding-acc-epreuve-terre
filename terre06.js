5baded01b04fce69717c226f342d3548fd1c2882
Dylan Tettarasar <dtettarasar@gmail.com>
2022-03-21 12:39:03 -0400

    terre06: created condition to test argument

--- terre06.js
+++ terre06.js
@@ -1,3 +1,25 @@
 // Exercice 7 : Inverser une chaîne
 
+const main = () => {
 
+    const argument = argTester();
+
+    console.log(argument);
+
+}
+
+const argTester = () => {
+
+    const args = process.argv.slice(2);
+
+    if (args.length != 1) {
+        console.log("Veuillez passer une chaîne de caractère en argument");
+        console.log("Exemple : node terre06.js 'From Mars to Sirius'");
+        return false;
+    }
+
+    return args;
+
+}
+
+main();
