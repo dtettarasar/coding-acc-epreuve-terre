eef3b7cedeae6dbb854524e75a5626d0f6516f23
Dylan Tettarasar <dtettarasar@gmail.com>
2022-03-24 22:02:26 +0100

    terre08 : update on argTester()

--- terre08.js
+++ terre08.js
@@ -11,6 +11,20 @@ const argTester = () => {
 
   const args = process.argv.slice(2);
 
+  const patternNegAndPos = /^-?[0-9]+$/;
+  const patternPosOnly = /^[0-9]+$/;
+
+  const firstNum = args[0];
+  const expos = args[1];
+
+  const testFirstNum = patternNegAndPos.test(firstNum);
+  const testExpos = patternPosOnly.test(expos);
+
+  if (args.length != 2 || !testFirstNum || !testExpos) {
+    console.log("error");
+    return false;
+  }
+
   return args;
 
 }
