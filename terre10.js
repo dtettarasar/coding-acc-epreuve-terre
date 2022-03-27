6808813ae451213eda4ad6d37e581da66bccf17b
Dylan Tettarasar <dtettarasar@gmail.com>
2022-03-27 06:06:45 -0400

    terre10 : argTester : check that we pass only one Arg

--- terre10.js
+++ terre10.js
@@ -11,6 +11,11 @@ const argTester = () => {
 
     const argument = process.argv.slice(2);
 
+    if (argument.length != 1) {
+        console.log("error");
+        return false;
+    }
+
     return argument;
 
 }
