8fbacec9547484e4f0fcbc571a399a183777b93d
Dylan Tettarasar <dtettarasar@gmail.com>
2022-03-29 18:25:49 +0200

    terre11 : update the regex in argTester()

--- terre11.js
+++ terre11.js
@@ -4,12 +4,23 @@ const main = () => {
 
     console.log("hello world");
     const argument = argTester();
-    console.log(argument);
+    // console.log(argument);
 }
 
 const argTester = () => {
 
     const argument = process.argv.slice(2);
+    const pattern = /^[0-9]{1,2}$/;    
+
+    if (argument.length != 1){
+
+        console.log("error");
+        return false;
+
+    }
+
+    console.log("test argument:");
+    console.log(pattern.test(argument[0]));
 
     return argument;
 
