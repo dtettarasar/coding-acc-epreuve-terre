c6eec83a3fe69c5e3a290bfc46cbe919ec96059b
Dylan Tettarasar <dtettarasar@gmail.com>
2022-04-02 20:02:03 +0200

    created terre13

---
+++ terre13.js
@@ -0,0 +1,19 @@
+// Exercice 14 : Trouver la Suisse
+
+const main = () => {
+
+}
+
+const argTester = () => {
+
+  const arguments = process.argv.slice(2);
+  console.log(arguments);
+
+  if (arguments.length != 3) {
+    console.log("error");
+    return false;
+  }
+
+}
+
+argTester();
