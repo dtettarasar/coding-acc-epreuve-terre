347100f7cd1304b2bc00c23920fe5348aa92d530
Dylan Tettarasar <dtettarasar@gmail.com>
2022-04-02 20:09:34 +0200

    terre13 : update on argTester

--- terre13.js
+++ terre13.js
@@ -7,11 +7,22 @@ const main = () => {
 const argTester = () => {
 
   const arguments = process.argv.slice(2);
+  const pattern = /^-?[0-9]+$/;
+
   console.log(arguments);
 
   if (arguments.length != 3) {
-    console.log("error");
+    console.log("error: wrong number of arguments");
     return false;
+  }
+
+  for (let i = 0; i < arguments.length; i++) {
+
+    if (!pattern.test(arguments[i])) {
+      console.log("error: at least one arg is not a number");
+      return false;
+    }
+
   }
 
 }
