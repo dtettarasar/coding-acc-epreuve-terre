5d66d4188dbc1ec02c97b217a13d4f16b9752d33
Dylan Tettarasar <dtettarasar@gmail.com>
2022-04-05 11:30:06 -0400

    terre14: created main func

--- terre14.js
+++ terre14.js
@@ -1,9 +1,19 @@
 // Exercice 15 : Trié ou pas
 
+const main = () => {
+
+    const arguments = argTester();
+
+    console.log(arguments);
+
+}
+
 const argTester = () => {
 
     const arguments = process.argv.slice(2);
     const pattern = /^-?[0-9]+$/;
+
+    const intArr = [];
 
     const errorMsg = "Veuillez passer en argument des nombres entiers (deux minimum).\nExemple : node terre14.js 12 45 789 3 22";
 
@@ -25,14 +35,21 @@ const argTester = () => {
                 console.log(errorMsg);
                 return false;
     
+            } else {
+
+                intArr.push(parseInt(arguments[i]));
+
             }
     
         }
 
     }
 
-    console.log(arguments);
+    // console.log(arguments);
+    // console.log(intArr);
+
+    return intArr;
 
 }
 
-argTester();
+main();
