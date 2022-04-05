870a3a3b6b0e50d17021729f50f202c0cb49e4b7
Dylan Tettarasar <dtettarasar@gmail.com>
2022-04-05 11:26:20 -0400

    terre14.js : update on ArgTester

--- terre14.js
+++ terre14.js
@@ -3,6 +3,33 @@
 const argTester = () => {
 
     const arguments = process.argv.slice(2);
+    const pattern = /^-?[0-9]+$/;
+
+    const errorMsg = "Veuillez passer en argument des nombres entiers (deux minimum).\nExemple : node terre14.js 12 45 789 3 22";
+
+    if (arguments.length < 2) {
+
+        console.log(errorMsg); 
+        return false;
+
+    } else {
+
+        for (let i = 0; i < arguments.length;i++) {
+
+            const argValid = pattern.test(arguments[i]);
+    
+            //console.log(arguments[i] + ": " + argValid);
+    
+            if (!argValid) {
+    
+                console.log(errorMsg);
+                return false;
+    
+            }
+    
+        }
+
+    }
 
     console.log(arguments);
 
