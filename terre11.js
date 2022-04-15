9ae751f8f397828a4361e5e87f4932511f342ea4
Dylan Tettarasar <dtettarasar@gmail.com>
2022-04-15 18:12:24 +0200

    terre11 : clean the file

--- terre11.js
+++ terre11.js
@@ -2,7 +2,6 @@
 
 const main = () => {
 
-    // console.log("hello world");
     const argument = argTester();
 
     if (argument) {
@@ -16,16 +15,11 @@ const main = () => {
 
 const convertTime = (time) => {
 
-    // console.log(time);
-
     let anteOrPostMeridiem = time.hours > 12 ? "PM" : "AM";
     let hoursTwelve = time.hours > 12 ? time.hours - 12 : time.hours;
     let minutesTwelve = time.minutes > 10 ? time.minutes : "0" + time.minutes;
 
     let timeTwelve = hoursTwelve + ":" + minutesTwelve + anteOrPostMeridiem;
-
-    // console.log("timeTwelve");
-    // console.log(timeTwelve);
 
     return timeTwelve;
 
@@ -46,17 +40,10 @@ const argTester = () => {
 
     }
 
-    // console.log("test argument:");
-    // console.log(pattern.test(argument[0]));
-
     const argArr = argument[0].split(":");
 
     time.hours = parseInt(argArr[0]);
     time.minutes = parseInt(argArr[1]);
-
-    // console.log(argArr);
-    // console.log("hours: " + time.hours);
-    // console.log("minutes: " + time.minutes);
     
     if (time.hours > 24 || time.minutes >= 60 || time.hours == 24 && time.minutes != 00) {
 
