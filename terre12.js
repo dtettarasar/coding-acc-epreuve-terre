f895022a511b965732730212fb89199527695948
Dylan Tettarasar <dtettarasar@gmail.com>
2022-04-17 17:22:23 +0200

    terre12 : update on comparison operators

--- terre12.js
+++ terre12.js
@@ -16,15 +16,15 @@ const convertTime = (time) => {
   let hourStr = null;
   let minuteStr = null;
 
-  if (time.meridiem == "pm" && time.hours != 12) {
+  if (time.meridiem === "pm" && time.hours !== 12) {
 
     hourStr = time.hours += 12;
 
-  } else if (time.meridiem == "am" && time.hours < 10) {
+  } else if (time.meridiem === "am" && time.hours < 10) {
 
     hourStr = "0" + time.hours;
 
-  } else if (time.meridiem == "am" && time.hours == 12) {
+  } else if (time.meridiem === "am" && time.hours === 12) {
 
     hourStr = "00";
 
@@ -47,7 +47,7 @@ const argTester = () => {
   const pattern = /^[0-9]{1,2}:[0-9]{2}(am|pm|AM|PM)$/;
   const time = {};
 
-  if (argument.length != 1 || !pattern.test(argument[0])) {
+  if (argument.length !== 1 || !pattern.test(argument[0])) {
 
       console.log("Veuillez passer en argument, une heure au format 12h");
       console.log("Exemple : node terre12.js 11:40PM");
@@ -61,7 +61,7 @@ const argTester = () => {
   time.minutes = parseInt(argArr[1].slice(0,2));
   time.meridiem = argArr[1].slice(2).toLowerCase();
 
-  if (time.hours > 12 || time.minutes >= 60 || time.hours == 0) {
+  if (time.hours > 12 || time.minutes >= 60 || time.hours === 0) {
     console.log("heure incorrecte");
     return false;
   }
